3de625e5a0aa7353c9fcf6a19ea84d263dad807b
Sunny Kumar <119757134+mailsunnyhere-ON-GIT@users.noreply.github.com>
2023-05-31 13:10:10 +0530

    Added solution - LeetHub

---
+++ Longest Sub-Array with Sum K - GFG/longest-sub-array-with-sum-k.java
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    String line = br.readLine();
+		    String[] element = line.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		    int K = Integer.parseInt(element[1]);
+		    
+		    int arr [] = new int[sizeOfArray];
+		    
+		    line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    int res = obj.lenOfLongSubarr(arr, sizeOfArray, K);
+		    
+		    System.out.println(res);
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    
+   
+    // Function for finding maximum and value pair
+    public static int lenOfLongSubarr (int A[], int N, int K) {
+        //Complete the function
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        int dist=0,sum=0;
+        hm.put(0,-1);
+        for(int i=0;i<N;i++)
+        {
+            sum=sum+A[i];
+            if(hm.containsKey(sum-K))
+            {
+                int x=i-hm.get(sum-K);
+                if(x>dist)
+                dist=x;
+            }
+            if(hm.containsKey(sum)){
+                continue;}
+            else
+            hm.put(sum,i);
+            
+        }
+        return dist;
+    }
+    
+    
+}
+
+
