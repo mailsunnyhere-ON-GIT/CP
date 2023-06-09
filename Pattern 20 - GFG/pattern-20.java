5d809b6c3639fbbd7246523fef55a852aa409210
Sunny Kumar <119757134+mailsunnyhere-ON-GIT@users.noreply.github.com>
2023-06-09 23:47:23 +0530

    Added solution - LeetHub

---
+++ Pattern 20 - GFG/pattern-20.java
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution obj = new Solution();
+            obj.printTriangle(n);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+
+    void printTriangle(int n) {
+        // code here
+        
+        
+        
+        
+        boolean flag=false;
+        int init=n*2-2;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            for(int k=0;k<init;k++)
+            {
+                System.out.print(" ");
+            }
+             for(int j=0;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            
+            System.out.println();
+           
+            init=init-2;
+            
+    }
+    
+    
+     init=2;
+        for(int i=n;i>1;i--)
+        {
+            for(int j=1;j<i;j++)
+            {
+                System.out.print("*");
+            }
+            for(int k=0;k<init;k++)
+            {
+                System.out.print(" ");
+            }
+             for(int j=1;j<i;j++)
+            {
+                System.out.print("*");
+            }
+            
+            System.out.println();
+           
+            init=init+2;
+            
+    }
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
