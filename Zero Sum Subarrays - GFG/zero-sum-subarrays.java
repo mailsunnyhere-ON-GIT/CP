565a81b72433931f8c8074be470999283a8a980a
Sunny Kumar <119757134+mailsunnyhere-ON-GIT@users.noreply.github.com>
2023-06-18 20:59:38 +0530

    Added solution - LeetHub

---
+++ Zero Sum Subarrays - GFG/zero-sum-subarrays.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		sc.nextLine();
+		while(t-->0)
+		{
+		    int n;
+		    n=sc.nextInt();
+		    
+		    long arr[]=new long[n];
+	
+		    
+		    for(int i=0;i<n;i++)
+		    {
+		        arr[i]=sc.nextLong();
+		    }
+            Solution ob = new Solution();
+		    System.out.println(ob.findSubarray(arr,n));
+		}
+		
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    //Function to count subarrays with sum equal to 0.
+    public static long findSubarray(long[] arr ,int n) 
+    {
+        //Your code here
+       HashMap<Long, Long> hm = new HashMap<>();
+
+        hm.put(0L,1L);
+         long sum=0L;
+        long count=0L;
+        for(long num:arr)
+        {
+           sum=sum+num;
+            if(hm.containsKey(sum)){
+            count=count+hm.get(sum);}
+            hm.put(sum,hm.getOrDefault(sum,0L)+1L);
+        }
+        return count;
+    }
+}
