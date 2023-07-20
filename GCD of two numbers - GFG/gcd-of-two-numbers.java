465c760a2cc5ffebf8305bd0ad8823ea9b3b1e56
Sunny Kumar <119757134+mailsunnyhere-ON-GIT@users.noreply.github.com>
2023-07-21 00:37:33 +0530

    Added solution - LeetHub

--- GCD of two numbers - GFG/gcd-of-two-numbers.java
+++ GCD of two numbers - GFG/gcd-of-two-numbers.java
@@ -28,7 +28,7 @@ class Solution
     public int gcd(int A , int B) 
     { 
       
-      if(B==0)
+      if(B==0) // bigger number should become 0 
       return A;
       else
       return gcd(B,A%B);
